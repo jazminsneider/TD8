01893a67dd16e61bdae743c51d23b39f25279972
tototomygallo <164676348+tototomygallo@users.noreply.github.com>
2025-09-16 09:44:39 -0300

    Update 07_split_in_tasks.py

--- Pablo/Crear Archivos/07_split_in_tasks.py
+++ Pablo/Crear Archivos/07_split_in_tasks.py
@@ -1,77 +1,45 @@
 import pandas as pd
-import logging
-import os
+import helper
 from tqdm import tqdm
-import glob
+import os.path
 
-def debug(*msg):
-    logger = logging.getLogger(__name__)
-    logger.debug(" ".join([str(x) for x in msg]))
-
-def read_list(filename, verbose=True):
-    if not os.path.exists(filename):
-        raise Exception("missing list: {}".format(filename))
-
-    file = open(filename, "r")
-    if verbose:
-        debug("reading {}".format(filename))
-
-    res = []
-    lines = file.readlines()
-    for line in lines:
-        if line.strip() != "":
-            chunks = line.split()
-            if len(chunks) > 1:
-                res.append(chunks)
-            else:
-                res.append(chunks[0])
-    return list(res)
-
-
-def save_list(lines, fname, verbose=True, separator="\t", append=False):
-    def _multiple_line(line):
-        return type(line) is list or type(line) is tuple or type(line) is np.ndarray
-    if append:
-        mode = "a"
-    else:
-        mode = "w"
-    with open(fname, mode) as fn:
-        for line in lines:
-            if _multiple_line(line):
-                fn.write(separator.join([str(l) for l in line]) + "\n")
-            else:
-                fn.write(line + "\n")
-    if verbose:
-        debug("saving {}".format(fname))
+z_scored_tracks_list="/home/gallin/Documentos/pablo/lists/z_scored_tracks.lst"
+tasks_table="/home/gallin/Documentos/pablo/tasks_uba.csv"
+output_list="/home/gallin/Documentos/pablo/lists/tasks_features.lst"
+output_folder="/home/gallin/Documentos/pablo/output/tasks_features/"
+force=False
 
 
 
-tasks = pd.read_csv('csvs/tasks_uba.csv')
+helper.mkdir_p(output_folder)
+helper.mkdir_p(os.path.dirname(output_list))
+
+tasks = pd.read_csv(tasks_table)
+# tasks = pd.read_csv(tasks_table)
 tokens_list = []
-output_folder = "tasks_features"
-input_folder = "standardized"
 
-for track_fname in tqdm(sorted(glob.glob(os.path.join(input_folder, "*.csv")))):
-        base_name = os.path.basename(track_fname)  # ej: "1B_wavs_standardized.csv"
-        name_without_ext = os.path.splitext(base_name)[0]  # "1B_wavs_standardized"
-        # Extraemos session y channel
-        first_part = name_without_ext.split("_")[0]  # "1B"
-        sess = first_part[:-1]  
-        channel = first_part[-1]  
-        tasks_for_session = tasks[tasks.session == int(sess)]
-        if channel == "A":
-            features_A = pd.read_csv(track_fname, index_col="time")
-            features_B = pd.read_csv(track_fname.replace("_A", "_B"), index_col="time")
+for sess_channel, track_fname in tqdm(helper.read_list(z_scored_tracks_list)):
+    sess, channel = sess_channel.split("_")
 
-            for _, task_info in tasks_for_session.iterrows():
-                task_id = task_info.task_number
+    tasks_for_session = tasks[tasks.session == int(sess)]
+    if channel == "A":
+        features_A = pd.read_csv(track_fname, index_col="time")
+        features_B = pd.read_csv(track_fname.replace("_A", "_B"), index_col="time")
 
-                sess_task = "{}_{}".format(sess, task_id)
-                output_fname = "{}/{}_{}.csv".format(output_folder, sess, task_id)
+        for _, task_info in tasks_for_session.iterrows():
+            task_id = task_info.task_number
+
+            sess_task = "{}_{}".format(sess, task_id)
+            output_fname = "{}/{}_{}.csv".format(output_folder, sess, task_id)
+
+            if not force and helper.exists(output_fname):
+                helper.warning("{} already exist, skipping (use --force to overwrite)".format(output_fname))
+            else:
                 features_task_A = features_A.loc[features_A.task == task_id, :].drop("task", axis=1)
                 features_task_B = features_B.loc[features_B.task == task_id, :].drop("task", axis=1)
                 joined = features_task_A.join(features_task_B, lsuffix="_A", rsuffix="_B")
                 joined.to_csv(output_fname)
-                tokens_list.append((sess_task, output_fname))
-                
-            save_list(tokens_list, "tasks_features.lst")
+
+            tokens_list.append((sess_task, output_fname))
+
+helper.save_list(tokens_list, output_list)
