f0ba90a3b52ef574410be02b69d68850e2579767
Jazmín <166775376+jazminsneider@users.noreply.github.com>
2025-10-15 13:01:01 -0300

    Update 03_add_hold_transitions.py

--- Pablo/Crear Archivos/03_add_hold_transitions.py
+++ Pablo/Crear Archivos/03_add_hold_transitions.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 
 ipus = pd.read_csv('csvs/ipus_uba.csv', index_col="token_id")
 tt = pd.read_csv('csvs/tt-table.csv')
+
 tt = tt[tt["tt_label"] != "H"]
 
 for name, group in tqdm(ipus.groupby(["session_number", "task"])):
@@ -19,4 +20,4 @@ for name, group in tqdm(ipus.groupby(["session_number", "task"])):
                     res.append(dict(tt_label="H", session_number=ipu2.session_number, speaker2=ipu2.channel, task=ipu2.task, ipu2_start_time=ipu2.ipu_start_time, ipu2_end_time=ipu2.ipu_end_time, overlapped_transition=False))
             tt = pd.concat([tt, pd.DataFrame(res)], ignore_index=True)
 tt.to_csv("csvs/holds-tt-table.csv", index=False)
-
+print(tt.tt_label.value_counts())
